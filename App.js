697c76320d4c6a7cac3bc8284bd03a932dff49b2
dabit3 <dabit3@gmail.com>
2022-12-22 07:00:25 -0600

    updated RNLens Version, added dark theme

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
+import { StyleSheet } from 'react-native';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { LensProvider } from '@lens-protocol/react-native-lens-ui-kit';
 
 import Profiles from './Profiles'
 import ViewProfile from './ViewProfile'
@@ -12,14 +12,16 @@ const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Profiles}  />
-        <Stack.Screen name="ViewProfile" component={ViewProfile} />
-        <Stack.Screen name="ViewComments" component={ViewComments} />
-        <Stack.Screen name="ViewFollowing" component={ViewFollowing} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <LensProvider theme="dark">
+      <NavigationContainer theme={DarkTheme}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Profiles}  />
+          <Stack.Screen name="ViewProfile" component={ViewProfile} />
+          <Stack.Screen name="ViewComments" component={ViewComments} />
+          <Stack.Screen name="ViewFollowing" component={ViewFollowing} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </LensProvider>
   );
 }
 
